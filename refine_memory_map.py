e8d411dc88f8f93cf266b32baee5ffec064aa6bd
Petey <pdotjpr@gmail.com>
2020-12-06 00:17:33 -0500

    memory graph refinement script

--- refine_memory_map.py
+++ refine_memory_map.py
@@ -1,6 +1,5 @@
 """
-Run the heap analysis code!
-Example: python harvest_heap_data.py 'gnome-terminal -- vim' --pgrep vim
+Run memory cartograhy multiple times and bulid a refined memory graph
 """
 
 import argparse
@@ -12,15 +11,13 @@ import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("cmd", type=str, help="the command to run and analyze as it would normally be typed into a shell")
-parser.add_argument("--outdir", type=str, default="heap_analysis_{}".format(datetime.datetime.now().strftime("%M%d%Y-%I%M")), help="directory to dump outputs")
-parser.add_argument("--num_repeats",type=int, default=5, help="Re-run the binary")
-parser.add_argument("--heap_region",type=str, default="[heap]", help="name of the memory region to analyze (as seen in proc/pid/maps)")
+parser.add_argument("--outdir", type=str, default="memory_map_{}".format(datetime.datetime.now().strftime("%M%d%Y-%I%M")), help="directory to memory outputs")
+parser.add_argument("--num_repeats",type=int, default=3, help="Re-run the binary")
 parser.add_argument("--attach_time",type=int, default=0, help="How long (in seconds) to wait before attaching and analyzing. If 0, await user input")
-parser.add_argument("--region_number",type=int, default=0, help="If there are multiple regions with the specfied name, this index specifies which one to use \
-                                                                (based on the order of the regions in /proc/pid/maps)")
 parser.add_argument("--pgrep",type=str, default="", help="expression to pgrep for and attach to. If none is provided, will just attach to the PID of the spawned subprocess.")
 args = parser.parse_args()
 
+mg = None
 os.makedirs(args.outdir, exist_ok=True)
 for i in range(args.num_repeats):
     child = subprocess.Popen(args.cmd.split(" ") + ["&"])
@@ -36,5 +33,23 @@ for i in range(args.num_repeats):
         pid = child.pid
 
     # dump the memory
-    os.system("sudo gdb -x cartography_gdb.py -ex 'py gdb_main({}, [\"{}_{}\"], True, \"{}\")'".format(pid, args.heap_region, args.region_number, args.outdir + "/run{}_".format(i)))
+    os.system("sudo gdb -x cartography_gdb.py -ex 'py gdb_main({}, name=\"{}\")'".format(pid, args.outdir + "/run{}_".format(i)))
     os.system("kill -9 {}".format(pid))
+
+    #refine the memory graph
+    with open(args.outdir + "/run{}_".format(i) + "memgraph.pickle", "rb") as f:
+        newmg = pickle.load(f)
+        if mg:
+            for src in mg.keys():
+                for dst in mg[src].keys():
+                    edgelist = mg[src][dst]
+                    newlist = []
+                    for e in edgelist:
+                        if e in newmg[src][dst]:
+                            newlist.append(e)
+                    mg[src][dst] = newlist
+        else:
+            mg = newmg
+
+with open(args.outdir + "/memgraph_final.pickle", "wb") as f:
+    pickle.dump(mg, f)
