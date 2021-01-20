deeab9d402597befa3bcb099b34248c10490915c
Petey <pdotjpr@gmail.com>
2021-01-20 17:20:32 -0500

    updates to refinement script

--- refine_memory_map.py
+++ refine_memory_map.py
@@ -17,10 +17,12 @@ parser.add_argument("--outdir", type=str, default="memory_map_{}".format(datetim
 parser.add_argument("--dump", dest='dump', action='store_true', help="Whether to actually run the program and dump the data. If not, will assume data is already present in outdir.")
 parser.add_argument("--num_repeats",type=int, default=3, help="Re-run the binary")
 parser.add_argument("--attach_time",type=int, default=0, help="How long (in seconds) to wait before attaching and analyzing. If 0, await user input")
+parser.add_argument("--pgrepuser",type=str, default="", help="owner of the sought process (ie, www-data for apache handlers)")
 parser.add_argument("--pgrepattach",type=str, default="", help="expression to pgrep for and attach to. If none is provided, will just attach to the PID of the spawned subprocess.")
 parser.add_argument("--pgrepkill",type=str, default="", help="expression to pgrep when killing processes. If not specified, kills process found with pgrep")
 parser.add_argument("--pointer_sz", type=int, default=8, help="Length of a pointer in memory being analyzed")
 parser.add_argument("--nograph", dest='nograph', action='store_true', help="Don't build out the graph. Just save the maplists and dumps and build the graph later")
+parser.add_argument("--killsig",type=int, default=9, help="Signal number to send for killing processes. Defaults to KILL")
 
 
 args = parser.parse_args()
@@ -28,14 +30,19 @@ args = parser.parse_args()
 os.makedirs(args.outdir, exist_ok=True)
 if args.dump:
     for i in range(args.num_repeats):
-        child = subprocess.Popen(args.cmd.split(" "))
+        print("Launching...")
+        child = subprocess.Popen(args.cmd, shell=True)
         if args.attach_time == 0:
             input("Press any key to pause and analyze memory...")
         else:
+            print("Pausing for {} seconds".format(args.attach_time))
             time.sleep(args.attach_time)
-        
+
         if len(args.pgrepattach) > 0:
-            proc = subprocess.Popen(['pgrep', args.pgrepattach], stdout=subprocess.PIPE)
+            if len(args.pgrepuser) > 0:
+                proc = subprocess.Popen(['pgrep', '-u', args.pgrepuser, args.pgrepattach], stdout=subprocess.PIPE)
+            else:
+                proc = subprocess.Popen(['pgrep', args.pgrepattach], stdout=subprocess.PIPE)
             pid = int(proc.stdout.read().decode().split("\n")[0])
         else:
             pid = child.pid
@@ -51,9 +58,9 @@ if args.dump:
 
         # determine who to kill
         if len(args.pgrepkill) > 0:
-            os.system("pkill -9 '{}'".format(args.pgrepkill))    
+            os.system("pkill -{} '{}'".format(args.killsig, args.pgrepkill))    
         else:
-            os.system("kill -9 {}".format(pid))    
+            os.system("kill -{} {}".format(args.killsig, pid))  
 
     print("Finished dumping memory! Refining graph...")
 
